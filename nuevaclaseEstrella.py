eac88f7c79362eea62d773e3e5f73dfc52b71337
monsezuni <monserrat.zuniga.mo@usach.cl>
2024-05-12 17:25:18 -0400

    Cálcula el tipo espectral de la estrella

---
+++ nuevaclaseEstrella.py
@@ -0,0 +1,64 @@
+import numpy as np
+class Estrella():
+    """
+    Clase que representa una estrella. Sus atributos principales son:
+
+    Atributos:
+    - nombre: El nombre de la estrella. (público)
+    - masa: La masa de la estrella. (protegido)
+    - radio: El radio de la estrella. (protegido)
+    - temperaturasuperficial: La temperatura superficial de la estrella. (protegido)
+    - distancia: La distancia de la estrella. (protegido)
+    - movimientopropio: El movimiento propio de la estrella. (protegido)
+    """
+
+    def __init__(self, nombre, masa, radio, temperaturasuperficial, distancia, movimientopropio):
+        self.nombre = nombre
+        self._masa = masa
+        self._radioestrella = radio
+        self._teff= temperaturasuperficial
+        self._distancia = distancia
+        self._movimientopropio= movimientopropio
+    
+    def luminosidad_total(self):
+        """
+        Calcula la luminosidad total de la estrella.
+
+        Returns:
+        - La luminosidad total de la estrella.
+        """
+        return float(4 * np.pi * (self._radioestrella**2) * self._teff)
+    
+    def luminosidad_secuencia_principal(self,l_sol, m_sol):
+        """
+        Calcula la luminosidad de la estrella en la secuencia principal.
+
+        Parámetros:
+        - l_sol: La luminosidad del Sol.
+        - m_sol: La masa del Sol.
+
+        Returns:
+        - La luminosidad de la estrella en la secuencia principal.
+        """
+        return  float( l_sol * (self._masa/m_sol)**3.5)
+    def tipo_espectral(self):
+        """
+        Calcula el tipo espectral de la estrella basado en su temperatura superficial.
+
+        Returns:
+        - El tipo espectral de la estrella.
+        """
+        if self._teff < 3500:
+            return "M"
+        elif self._teff < 5000:
+            return "K"
+        elif self._teff < 6000:
+            return "G"
+        elif self._teff < 7500:
+            return "F"
+        elif self._teff < 10000:
+            return "A"
+        elif self._teff < 30000:
+            return "B"
+        else:
+            return "O"
